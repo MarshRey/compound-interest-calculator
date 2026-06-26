2d935929cc7881adfaedd370e291ac667bdd6c1d
MarshRey <113206085+MarshRey@users.noreply.github.com>
2026-06-26 10:58:55 -0700

    Update domain to compoundgrowcalculator.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://compoundinterestcalculator.example.com',
+  site: 'https://compoundgrowcalculator.com',
   output: 'static',
   integrations: [sitemap(), tailwind()],
 });
